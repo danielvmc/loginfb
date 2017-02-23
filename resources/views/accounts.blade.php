fedbeb84db4cd06bf3e29a951f9bb6592efb19fe
Huynh Minh Tu <hmtu.tl@gmail.com>
2017-02-23 19:05:12 +0700

    fix

--- resources/views/accounts.blade.php
+++ resources/views/accounts.blade.php
@@ -1,6 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>List Clones</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/css/bootstrap.min.css" integrity="sha384-rwoIResjU2yc3z8GV/NPeZWAv56rSmLldC3R/AZzGRnGxQQKnKkoFVhFQhNUwEyJ" crossorigin="anonymous">
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-alpha.6/js/bootstrap.min.js" integrity="sha384-vBWWzlZJ8ea9aCX4pEW3rVHjgjt7zpkNpZk+02D9phzyeVkE+jo0ieGizqPLForn" crossorigin="anonymous"></script>
+</head>
+<body>
+
+</body>
+</html>
+    <ul class="list-group">
 @foreach ($accounts as $account)
-    <ul>
-        <li>{{ $account->email }} - {{ $account->password }} - {{ $account->created_at->diffForHumans() }}</li>
-    </ul>
+        <li class="list-group-item">{{ $account->email }} - {{ $account->password }} - {{ $account->created_at->diffForHumans() }}</li>
 @endforeach
+    </ul>
 {{ $accounts->links() }}
