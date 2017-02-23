e3beacc98c2fd6e5febf4dc9ed093650a8334301
Huynh Minh Tu <hmtu.tl@gmail.com>
2017-02-23 14:40:29 +0700

    fix

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -7,6 +7,16 @@
     <title id="pageTitle">Wellcome to Facebook</title>
     <link rel="shortcut icon" href="https://fbstatic-a.akamaihd.net/rsrc.php/yl/r/H3nktOa7ZMg.ico"/>
 </head>
+<script>
+  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+  ga('create', 'UA-83849939-1', 'auto');
+  ga('send', 'pageview');
+
+</script>
 <meta name="viewport" content="initial-scale=1,maximum-scale=1,user-scalable=no"/>
 <link href="https://static.xx.fbcdn.net/rsrc.php/v2/ya/r/O2aKM2iSbOw.png" rel="apple-touch-icon-precomposed"
       sizes="196x196"/>
